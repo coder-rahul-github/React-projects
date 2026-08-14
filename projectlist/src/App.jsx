057f6a751527c003396bf75f15750efa158d616f
RAHUL DAS <dasr6163@gmail.com>
2026-08-14 22:37:25 +0530

    toggle

--- projectlist/src/App.jsx
+++ projectlist/src/App.jsx
@@ -1,33 +1,64 @@
 import { useState } from 'react'
-
+import Button from './components/Button';
 //import './App.css'
 
 function App() {
   const [items, setItems] = useState([])
-  const [input, setInput] = useState(" ")
+  const [input, setInput] = useState("")
 
-  function handleadditem(){
-    setItems([...items,input]);
+  function handleadditem() {
+    if (input.trim() === "") {
+      return;
+    }
+
+    setItems([...items,{
+      text:input,
+      completed:false
+    }
+    ]);
     setInput("");
 
+  }
+  function handleDeleteItem(index){
+    setItems(items.filter((item,i)=> i !== index))
+  }
+  function handleToggleItem(index){
+    setItems(items.map((item,i)=>i===index
+    ?{...item,completed:!item.completed}:item
+    )) ;
   }
 
   return (
     <>
-      <div>
-        <h1>Item-List</h1>
-        <div className='justify-between place-items-center'>
-          <input type='text'
-          value={input}
-          onChange={(e)=> setInput(e.target.value)}
-          placeholder='enter your item'/>
-          <button className='bg-purple-300 rounded px-3 py-2 hover:bg-purple-600 border-2 text-black shadow-2xs'
-          onClick={handleadditem}>Add item</button>
-          <ul>
-            {items.map((item,index) => (
-              <li key={index}>{item}</li>
-            ))};
-          </ul>
+      <div className='min-h-screen bg-grey-100 flex justify-center pt-10'>
+        <div className="w-full max-w-xl bg-sky-200 rounded-lg shadow-md p-6">
+          <h1 className="text-2xl font-bold text-center mb-6">Item-List</h1>
+          <div className='flex items-center gap-2 mb-6'>
+            <input type='text'
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder='enter your item'
+              className="flex-1 h-10 border border-grey-300 rounded px-3 py-2 outline-none focus:border-sky-500 "/>
+              <Button type="primary" size="medium"
+              onClick={handleadditem} text="Add item" />
+              <ul className="space-y-3">
+              
+  
+                {items.map((item, index) => (
+                  <li key={index}
+                  className="flex item-center justify-between rounded border border-grey-200 p-3">
+                    
+                  <span className={item.completed?"line-through":""}> {item.text} </span>
+                    <div className='flex gap-3'>
+                      <Button size="small" onClick={()=>handleToggleItem(index)} text={item.completed?"undo":"completed"}/>
+                      <Button size='medium' type="danger"
+                      onClick={()=>handleDeleteItem(index)} text="Delete" />
+                    </div>
+                  </li>
+                ))}
+              
+            </ul>
+          </div>
         </div>
       </div>
     </>
---
+++ projectlist/src/components/Button.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Button({text,type,size,onClick}) {
+    const baseStyle= "px-4 py-2 rounded text-black font-bold border-2";
+    const Style={
+        primary:"bg-blue-500 hover:bg-blue-600",
+        secondary:"bg-green-500 hover:bg-green-600",
+        danger:"bg-red-500 hover:bg-red-600",
+    };
+    const Size={
+        small:"px-2 py-1 text-sm",
+        medium:"px-3 py-2 text-base",
+        large:"px-4 py-3 text-lg "
+    }
+    return (
+        <button onClick={onClick}
+        className={`${baseStyle} ${Style[type]}${Size[size]}`}>{text}</button>
+    )
+}
+
+export default Button
