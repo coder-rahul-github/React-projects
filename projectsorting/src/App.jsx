e34932b9a34a70b5aa05919daabdb9686078d29e
RAHUL DAS <dasr6163@gmail.com>
2026-08-28 17:50:01 +0530

    expence tracker initial code

---
+++ projectsorting/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <table>
+        <thead>
+        <tr className="table-header flex gap-2 justify-center">
+          <th>Movie</th>
+          <th>rating</th>
+          <th>date</th>
+        </tr>
+        </thead>
+        <tr>
+          
+        </tr>
+        <tr>
+          
+          
+        </tr>
+      </table>
+      </div>
+    </>
+  )
+}
+
+export default App
