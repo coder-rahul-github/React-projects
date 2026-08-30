f079bfaac1a814457b15a4788ec7eb01aaa189cb
RAHUL DAS <dasr6163@gmail.com>
2026-08-30 23:22:45 +0530

    add total function

--- projectsorting/src/App.jsx
+++ projectsorting/src/App.jsx
@@ -2,21 +2,30 @@ import { useState } from "react";
 import Expenceform from "./component/Expenceform"
 
 function App() {
-//   const [expenses,setExpenses]=useState([]);
-//   const[selectedcategory,setSelectedcategory]=useState("All");
-//   const [sortorder,setSortorder]=useState("asc");
-//   function addExpenses(newExpenses){
-//     const expense={
-//       id:Date.now(),
-//       ...newExpenses
-//     };
-//     setExpenses((previousExpenses)=>[
-//       ...previousExpenses,expense,
-//     ]);
-//   }
-//   //filter expenses
-//   const filterexpenses= selectedCategory==="All"?expenses:expenses.filter((expense)=>expenses.category===selectedCategory);
-
+  const [expenses,setExpenses]=useState([]);
+  const[selectedCategory,setSelectedCategory]=useState("All");
+  const [sortOrder,setSortOrder]=useState("asc");
+  function addExpenses(newExpenses){
+    const expense={
+      id:Date.now(),
+      ...newExpenses
+    };
+    setExpenses((previousExpenses)=>[
+      ...previousExpenses,expense,
+    ]);
+  }
+  //filter expenses
+  const filterExpenses= selectedCategory==="All"?expenses:expenses.filter((expense)=>expenses.category===selectedCategory);
+  // sort expenses
+  const sortingExpenses=[...filterExpenses].sort((a,b)=>{
+    if (sortOrder==="asc"){
+      return a.amount-b.amount;
+    }
+    return b.amount-a.amount;
+  });
+  //sum total
+  const total=expenses.reduce((sum,expense)=>sum+expense.amount,0);
+  
   return (
     <>
       <div className="max-w-6xl mx-auto p-10">
